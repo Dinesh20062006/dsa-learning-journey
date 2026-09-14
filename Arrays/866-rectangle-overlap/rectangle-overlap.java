e8300b5595ada2fd7b5a03b87a898ecbd00a311f
Dinesh S <dineshsaravanan2010@gmail.com>
2026-09-14 18:05:37 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (52.92%) - LeetSync

---
+++ Arrays/866-rectangle-overlap/rectangle-overlap.java
@@ -0,0 +1,5 @@
+class Solution {
+    public boolean isRectangleOverlap(int[] rect1, int[] rect2) {
+        return !(rect1[2]<=rect2[0]||rect1[3]<=rect2[1]||rect1[0]>=rect2[2]||rect1[1]>=rect2[3]);
+    }
+}
